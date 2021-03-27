7fefd975970bc1f2b7bdabd8abe8b98d1220c697
Vuean <hzhlxy@outlook.com>
2021-03-27 19:51:36 +0800

    feat(all):add file;

--- Chapter4/Chapter4Exe/Sec7Exe1HowManyPAT/main.cpp
+++ Chapter4/Chapter4Exe/Sec7Exe1HowManyPAT/main.cpp
@@ -1,4 +1,46 @@
+/*
+题意：字符串APPAPT 中包含了两个“PAT”，其中第一个PAT是第二位(P)，
+第四位(A)，第六位(T)；第二个PAT是第三位(P)，第四位(A)，第六位(T)。
+现给定字符串， 问一共可以形成多少个PAT?
+*/
+
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+const int MAXN = 100010;
+const int MOD = 1000000007;
+char str[MAXN]; // 字符串
+int leftNumP[MAXN] = { 0 }; // 每一位左边（含） P 的个数
+
 int main()
 {
+	gets_s(str);
+	int len = strlen(str);
+	for (int i = 0; i < len; i++)
+	{
+		if (i > 0)
+		{
+			leftNumP[i] = leftNumP[i - 1]; // 继承上一位的结果
+		}
+		if (str[i] == 'P')
+		{
+			leftNumP[i]++;
+		}
+	}
 
+	int ans = 0, rightNumT = 0;
+	for (int i = len - 1; i >= 0; i--)
+	{
+		if (str[i] == 'T')
+		{
+			rightNumT++;
+		}
+		else if (str[i] == 'A')
+		{
+			ans = (ans + leftNumP[i] * rightNumT) % MOD;
+		}
+	}
+	cout << "Ans = " << ans;
 }
--- Chapter4/Chapter4Exe/Sec7Exe2QuickSort/main.cpp
+++ Chapter4/Chapter4Exe/Sec7Exe2QuickSort/main.cpp
@@ -1,4 +1,51 @@
+/*
+题目描述：
+著名的快速排序算法里有一个经典的划分过程：通常采用某种方法取一个元素作为主元，
+通过交换，把比主元小的元素放到其左边，把比主元大的元素放到其右边。给定划分后的N
+个互不相同的正整数的排列，请问有多少个阮素可能是划分前选取的主元？
+*/
+
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+const int MAXN = 100010;
+const int INF = 0x3fffffff; // `一个很大的数
+
+// a为序列，leftMax和rightmin分别为左边最大的数和右边最小的数
+int a[MAXN], leftMax[MAXN], rightMin[MAXN];
+// ans记录所有主元，num为主元个数
+int ans[MAXN], num = 0;
+
 int main()
 {
-
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	leftMax[0] = 0;
+	for (int i = 1; i < n; i++)
+	{
+		leftMax[i] = max(leftMax[i - 1], a[i-1]);
+	}
+	rightMin[n - 1] = INF;	// A[n-1]右边没有比它小的数
+	for (int i = n - 2; i >= 0; i--)
+	{
+		rightMin[i] = min(rightMin[i + 1], a[i + 1]);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		if (leftMax[i] < a[i] && rightMin[i] > a[i])
+		{
+			ans[num++] = a[i];	// 记录主元
+		}
+	}
+	cout << "NUM = " << num << endl;
+	for (int i = 0; i < num; i++)
+	{
+		cout << ans[i] << " ";
+	}
+	cout << endl;
 }
