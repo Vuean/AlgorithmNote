cfbe67b58796481220d45bd9a1c41c01af5c0d95
Vuean <hzhlxy@outlook.com>
2021-04-16 22:09:54 +0800

    feat(all):add file;

--- Chapter6/Chapter6Exe/Sec4Exe3SpeechPatterns/main.cpp
+++ Chapter6/Chapter6Exe/Sec4Exe3SpeechPatterns/main.cpp
@@ -1,4 +1,55 @@
+/*
+令“单词” 的定义为大小写字母、数字的组合。给出一个字符串， 问出现次数最多
+的单词及其出现次数（一切除了大小写字母、数字之外的字符都作为单词的分隔符）。
+其中字母不区分大小写，且最后按小写字母输出。
+*/
+
+#include <iostream>
+# include <string>
+# include <map>
+using namespace std;
+
+bool check(char c) {	// 检查字符c是否是[0,9], [a,z], [A,Z]
+	if (c >= '0' && c <= '9') return true;
+	if (c >= 'a' && c <= 'z') return true;
+	if (c >= 'A' && c <= 'Z') return true;
+	return false;
+}
+
 int main()
 {
-
+	map<string, int> count;
+	string str;
+	getline(cin, str);	// 读入整行字符串
+	int i = 0;
+	while (i < (int)str.length()) {
+		string word;
+		while (i < (int)str.length() && check(str[i])) {
+			if (str[i] >= 'A' && str[i] <= 'Z') {
+				str[i] += 32;	// 将大写字母转化为小写字母
+			}
+			word += str[i];
+			i++;
+		}
+		if (word != "") {
+			if (count.find(word) == count.end()) {
+				count[word] = 1;
+			}
+			else {
+				count[word]++;
+			}
+		}
+		while (i < (int)str.length() && !check(str[i])) {
+			i++;
+		}
+	}
+	string ans;
+	int MAX = 0;
+	for (auto iter = count.begin(); iter != count.end(); iter++) {
+		if (iter->second > MAX) {
+			ans = iter->first;
+			MAX = iter->second;
+		}
+	}
+	cout << ans << " " << MAX << endl;
 }
--- Chapter6/Chapter6Exe/Sec4Exe4DigitalLibary/main.cpp
+++ Chapter6/Chapter6Exe/Sec4Exe4DigitalLibary/main.cpp
@@ -1,4 +1,59 @@
+/*
+给出N本书的编号、书名、作者、关键词（可能有多个）、出版社及出版年份，
+并给出M个查询，每个查询给出书名、作者、关键词（单个）、出版社及
+出版年份中的一个，要求输出满足该给出信息的所有书的编号。
+*/
+#include <iostream>
+#include <cstdio>
+#include <map>
+#include <set>
+#include <string>
+using namespace std;
+
+map<string, set< int>> mpTitle, mpAuthor, mpKey, mpPub, mpYear;
+
+// 在map中查找str
+void query(map<string, set<int>>& mp, string& str) {
+	if (mp.find(str) == mp.end()) cout << "Not Found" << endl;
+	else {
+		for (auto iter = mp[str].begin(); iter != mp[str].end(); iter++) {
+			printf_s("%07d\n", *iter);
+		}
+	}
+}
+
 int main()
 {
-
+	int n, m, id, type;
+	string title, author, key, pub, year;
+	scanf_s("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf_s("%d", &id);
+		char c = getchar();
+		getline(cin, title);
+		mpTitle[title].insert(id);
+		getline(cin, author);
+		mpAuthor[author].insert(id);
+		while (cin >> key) {
+			mpKey[key].insert(id);
+			c = getchar();
+			if (c == '\n') break;
+		}
+		getline(cin, pub);
+		mpPub[pub].insert(id);
+		getline(cin, year);
+		mpYear[year].insert(id);
+	}
+	string temp;
+	scanf_s("%d", &m);
+	for (int i = 0; i < m; i++) {
+		scanf_s("%d: ", &type);
+		getline(cin, temp);
+		cout << type << ": " << temp << endl;
+		if (type == 1) query(mpTitle, temp);
+		else if (type == 2) query(mpAuthor, temp);
+		else if (type == 3) query(mpKey, temp);
+		else if (type == 4) query(mpPub, temp);
+		else query(mpYear, temp);
+	}
 }
