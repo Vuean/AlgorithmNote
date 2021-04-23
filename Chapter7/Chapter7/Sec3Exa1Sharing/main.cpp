826d9ea00b80ee813e7486a790382b8b4641e7bd
Vuean <hzhlxy@outlook.com>
2021-04-23 12:52:29 +0800

    feat(all):add file;

--- Chapter7/Chapter7/Sec3Exa1Sharing/main.cpp
+++ Chapter7/Chapter7/Sec3Exa1Sharing/main.cpp
@@ -1,4 +1,46 @@
+/*
+给出两条链表的首地址以及若干结点的地址、数据、下一个结点的地址，
+求两条链表的首个共用结点的地址。如果两条链表没有共用结点，则输出－1。
+*/
+#include <cstdio>
+#include <string>
+using namespace std;
+
+const int maxn = 100010;
+
+struct NODE {
+	char data;	// 数据域
+	int next;	// 指针域
+	bool flag;	// 结点是否在第一条链中出现过
+}node[maxn];
+
 int main()
 {
+	for (int i = 0; i < maxn; i++) {
+		node[i].flag = false;
+	}
 
+	int s1, s2, n;	// s1与s2 分别代表两条链表的首地址
+	scanf_s("%d%d%d", &s1, &s2, &n);
+	int address, next;;	// 结点地址与后继结点地址
+	char data;
+	for (int i = 0; i < n; i++) {
+		scanf_s("%d %c %d", &address, &data, &next);
+		node[address].data = data;
+		node[address].next = next;
+	}
+	int p;
+	for (p = s1; p != -1; p = node[p].next)
+	{
+		node[p].flag = true;
+	}
+	for (p = s2; p != -1; p = node[p].next) {
+		if (node[p].flag == true) break;
+	}
+	if (p != -1) {
+		printf("%05d\n", p);
+	}
+	else {
+		printf("-1\n");
+	}
 }
--- Chapter7/Chapter7/Sec3Exa2LinkedListSorting/main.cpp
+++ Chapter7/Chapter7/Sec3Exa2LinkedListSorting/main.cpp
@@ -1,4 +1,60 @@
+/*
+给出N个结点的地址address、数据域data以及指针域next,然后给出链表
+的首地址，要求把在这个链表上的结点按data值从小到大输出。
+(可能存在无效结点：不在链表上的结点)
+*/
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+const int maxn = 100005;
+
+struct Node {
+	int address, data, next;
+	bool flag;
+}node[maxn];
+
+bool cmp(Node a, Node b) {
+	if (a.flag == false || b.flag == false) {
+		return a.flag > b.flag;	// 只要a 和b 中有一个无效结点， 就把它放后面去
+	}
+	else {
+		return a.data < b.data;	// 如果都是有效结点， 则按要求排序
+	}
+}
+
 int main()
 {
+	for (int i = 0; i < maxn; i++) {
+		node[i].flag = false;
+	}
 
+	int n, begin, address;
+	scanf_s("%d%d", &n, &begin);
+	for (int i = 0; i < n; i++) {
+		scanf_s("%d", &address);
+		scanf_s("%d%d", &node[address].data, &node[address].next);
+		node[address].address = address;
+	}
+	int count = 0, p = begin;
+	while (p != -1) {
+		node[p].flag = true;
+		count++;
+		p = node[p].next;
+	}
+	if (count == 0) {
+		printf("0, -1");
+	}
+	else {
+		// 筛选有效结点，并按data 从小到大排序（步骤4 )
+		sort(node, node + maxn, cmp);
+		printf("%d %05d\n", count, node[0].address);
+		for (int i = 0; i < count; i++) {
+			if (i != count - 1) {
+				printf("%05d %d %05d", node[i].address, node[i].data, node[i + 1].address);
+			}
+			else {
+				printf("%05d %d -1", node[i].address, node[i].data);
+			}
+		}
+	}
 }
